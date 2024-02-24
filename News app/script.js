32794774161c2009ad04a030378f1ff6bd30d187
Abdullah Al Mahdi <97908670+BlackNinjaJahin@users.noreply.github.com>
2024-02-25 02:40:57 +0600

    Add files via upload

---
+++ News app/script.js
@@ -0,0 +1,75 @@
+const apiKey = 'd8d62608c98e4a8fa1b914e6f00e0387';
+const blogContainer = document.getElementById('blog-container');
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+async function fetchRandomNews() {
+  try {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&apiKey=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error("Error Fetching random news", error);
+    return [];
+  }
+}
+
+async function fetchNewsQuery(query) {
+  try {
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error("Error Fetching by query", error);
+    return [];
+  }
+}
+//searching code 
+searchButton.addEventListener("click", async () => {
+  const query = searchField.value.trim();
+
+  if (query !== "") {
+    try {
+      const articles = await fetchNewsQuery(query);
+      displayBlogs(articles);
+    } catch (error) {
+      console.log("Error Fetching by query", error);
+    }
+  }
+});
+
+function displayBlogs(articles) {
+  blogContainer.innerHTML = "";
+  articles.forEach(article => {
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
+    const img = document.createElement("img");
+    img.src = article.urlToImage;
+    img.alt = article.title;
+    const title = document.createElement("h2");
+    const truncatedTitle = article.title.length > 30 ? article.title.slice(0, 30) + "....." : article.title;
+    title.textContent = truncatedTitle;
+    const description = document.createElement("p");
+    const truncateDes = article.description.length > 120 ? article.description.slice(0, 120) + "......." : article.description;
+    description.textContent = truncateDes;
+    blogCard.appendChild(img);
+    blogCard.appendChild(title);
+    blogCard.appendChild(description);
+    blogCard.addEventListener('click', () => {
+      window.open(article.url, "_blank");
+    });
+    blogContainer.appendChild(blogCard);
+  });
+}
+
+(async () => {
+  try {
+    const articles = await fetchRandomNews();
+    displayBlogs(articles);
+    console.log(articles);
+  } catch (error) {
+    console.error("Error fetch", error);
+  }
+})();
